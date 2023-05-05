a5de7c814ca84cc161538ee834a42d5e8f0707dd
Levente Ibrányi <ibranyilevente@gmail.com>
2023-05-06 00:44:33 +0200

    Initial commit

---
+++ src/test/java/com/actions/test/ActionBasicTests.java
@@ -0,0 +1,11 @@
+package com.actions.test;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class ActionBasicTests {
+    @Test
+    void actionFirstTest(){
+        Assertions.assertTrue(true);
+    }
+}
---
+++ src/test/java/com/actions/test/TestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.actions.test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
